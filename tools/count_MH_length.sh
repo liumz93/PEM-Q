1900d20a23661045a9ae0ffa4b2f25142cb0be00
Mengzhu Liu <39579997+liumz93@users.noreply.github.com>
2021-06-29 11:04:17 +0800

    Delete count_MH_length.sh

--- tools/count_MH_length.sh
+++
@@ -1,8 +0,0 @@
-awk -F "\t" '{if ($10 == "chr19" && length($15) == 0) {print $15}}' YJ051a_transloc_all.tab | wc -l
-awk -F "\t" '{if ($10 == "chr19" && length($15) == 1) {print $15}}' YJ051a_transloc_all.tab | wc -l
-awk -F "\t" '{if ($10 == "chr19" && length($15) == 2) {print $15}}' YJ051a_transloc_all.tab | wc -l
-awk -F "\t" '{if ($10 == "chr19" && length($15) == 3) {print $15}}' YJ051a_transloc_all.tab | wc -l
-awk -F "\t" '{if ($10 == "chr19" && length($15) >= 4 && length($15) <= 5) {print $15}}' YJ051a_transloc_all.tab | wc -l
-awk -F "\t" '{if ($10 == "chr19" && length($15) >=6 && length($15) <= 10) {print $15}}' YJ051a_transloc_all.tab | wc -l
-awk -F "\t" '{if ($10 == "chr19" && length($15) >=11 && length($15) <= 25) {print $15}}' YJ051a_transloc_all.tab | wc -l
-awk -F "\t" '{if ($10 == "chr19" && length($15) >25) {print $15}}' YJ051a_transloc_all.tab | wc -l
